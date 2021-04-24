355086e393a4a6f657573274e271844a1270bfee
Jeannie <jntsao@gmail.com>
2021-04-24 08:00:30 -0700

    created a search function and used && operator instead of two if statements in the add function

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,17 @@ let pokemonRepository = (function (){
     // define pokemonList
     let pokemonList = [];
     pokemonList = [
-        { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison']},
+        { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
     
-        { name: 'Oddish', height: 0.5, weight: 5.4, types: ['grass', 'poison']},
+        { name: 'Oddish', height: 0.5, weight: 5.4, types: ['grass', 'poison'] },
     
-        { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire']},
+        { name: 'Charmander', height: 0.6, weight: 8.5, types: ['fire'] },
     
-        { name: 'Squirtle', height: 0.5, weight: 9, types: ['water']},
+        { name: 'Squirtle', height: 0.5, weight: 9, types: ['water'] },
     
-        { name: 'JigglyPuff', height: 0.5, weight: 5.5, types: ['fairy', 'normal']},
+        { name: 'JigglyPuff', height: 0.5, weight: 5.5, types: ['fairy', 'normal'] },
     
-        { name: 'Charizard', height: 1.7, weight: 90.5, types: ['fire', 'flying']}
+        { name: 'Charizard', height: 1.7, weight: 90.5, types: ['fire', 'flying'] }
     ];
 
     // returns pokemon list
@@ -26,40 +26,29 @@ let pokemonRepository = (function (){
     // add a pokemon
     function add(pokemonItem){
         // check that the item being added is an object
-        if( typeof pokemonItem === 'object' ){
-            // every() method will check if all elements in the array pass the test. The test is "key in pokemonItem" which uses the in operator that returns true if the properties in pokemonItem are in specified object "pokemonList[0]" 
-            // Object.keys creates an array of the keys in the object pokemonList[0] 
-            let checkKeys = (key) => key in pokemonItem;
-            if( Object.keys(pokemonList[0]).every(checkKeys) ){
-                console.log("checked keys");
-                console.log(pokemonList[0]);
-                // push element into object if conditions are met
-                pokemonList.push(pokemonItem);
-            }
+        // every() method will check if all elements in the array pass the test. The test is "key in pokemonItem" which uses the in operator that returns true if the properties in pokemonItem are in specified object "pokemonList[0]" 
+        // Object.keys creates an array of the keys in the object pokemonList[0] 
+        if( typeof pokemonItem === 'object' && Object.keys(pokemonList[0]).every( key => key in pokemonItem) ){
+            // push object into array if conditions are met
+            pokemonList.push(pokemonItem);
+        }else{
+            console.log('Can\'t add Pokemon, information isn\'t valid.');
         }
     }
 
     // search for a pokemon
-    function searchPokerep(pokemonSearched){
-        let filterPokemon = pokemonList.filter(pokemonName => pokemonName.name.toLowerCase().includes(pokemonSearched.toLowerCase));
-        console.log(pokemonSearched + ' was found in the pokedex');
+    searchPokemon = (searchedPokemon) => {
+        return Object.keys(pokemonList).filter(key => {
+            return (pokemonList[key].name.toLowerCase()).includes(searchedPokemon.toLowerCase());
+        })
     }
 
     return{
-        searchPokerep: searchPokerep,
+        searchPokemon: searchPokemon,
         getAll: getAll,
         add: add
     };
 })();
-
-// someValues.forEach((element) => {
-//     console.log(element);
-// });
-
-// someValues.forEach((element, index) => {
-//     console.log(`Current index: ${index}`);
-//     console.log(element);
-// });
 
 // add a object with the valid keys
 pokemonRepository.add({
@@ -76,13 +65,19 @@ pokemonRepository.add({
 //     weight: 7
 // });
 
-// find pokemon by name, display in console whether or not pokemon was found in pokemon repository. use filter function
-pokemonRepository.searchPokerep('Oddish');
+// someValues.forEach((element) => {
+//     console.log(element);
+// });
+
+// someValues.forEach((element, index) => {
+//     console.log(`Current index: ${index}`);
+//     console.log(element);
+// });
 
 // print out pokemon onto page
 let pokemonList = pokemonRepository.getAll();
 
-pokemonList.forEach((pookemonItem, i) => {
+pokemonList.forEach((pokemonItem, i) => {
     document.write('<p class = "pokeText">' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
     // conditional to check if height is above a certain value
     if(pokemonList[i].height > 1){
@@ -90,3 +85,9 @@ pokemonList.forEach((pookemonItem, i) => {
     }
     document.write('</p>' + '<br>');
 });
+
+// find pokemon by name, display in console whether or not pokemon was found in pokemon repository. use filter function
+// if the searched pokemon exists, then return array information about it
+console.log(searchPokemon('squirtle'));
+// if it doesn't return an empty array
+console.log(searchPokemon('DNE'));
