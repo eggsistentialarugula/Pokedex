670a78bb6416893ef7981eb37cc5030c367ccbe7
Jeannie <jntsao@gmail.com>
2021-05-29 00:47:04 -0700

    created a go to top of page function

--- js/scripts.js
+++ js/scripts.js
@@ -172,4 +172,9 @@ pokemonRepository.loadList().then(function() {
     });
 });
 
+// credits: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+function topFunction(){
+  document.body.scrollTop = 0; //safari
+  document.documentElement.scrollTop = 0; //for chrome, firefox, IE, and opera  
+}
 
