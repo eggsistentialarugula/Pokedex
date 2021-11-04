d2985ab1b20dbac12ee25bb91da1c91b84bf5e2c
Jeannie <jntsao@gmail.com>
2021-11-04 02:02:49 -0700

    used hidden.bs.modal to detect when someone closes the modal and to reset color of background depending on pokemon type

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-undef */
 /* eslint-disable quotes */
 ///////// assign  an IIFE variable ////////////
-let pokemonRepository = (function (){
+let pokemonRepository = (function () {
   // create empty array of pokemon
   let pokemonList = [];
   // api variable
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   //add pokemon to the empty array of pokemon
-  function add(pokemonItem){
+  function add(pokemonItem) {
     // check that the item being added is an object
-    if(typeof pokemonItem === 'object'){
-        // push object into array if conditions are met
-        pokemonList.push(pokemonItem);
-    }else{
-        console.log('Can\'t add Pokemon, information isn\'t valid.');
+    if (typeof pokemonItem === 'object') {
+      // push object into array if conditions are met
+      pokemonList.push(pokemonItem);
+    } else {
+      console.log('Can\'t add Pokemon, information isn\'t valid.');
     }
   }
 
   // return the pokemon list
-  function getAll(){
+  function getAll() {
     return pokemonList;
   }
 
@@ -62,32 +62,46 @@ let pokemonRepository = (function (){
         pokemonItem.abilities.push(" " + abilities.ability.name);
       });
 
+      console.log(pokemonItem.types[0]);
+
+      //create class for types in to style modal border by color
+      let types = pokemonItem.types[0];
+      $(".modal-content").addClass(`${types.toLowerCase()}-bg`);
+
+      // catch the event the user closes the modal (no matter how the user closes the modal)
+      $("#exampleModalCenter").on("hidden.bs.modal", function () {
+        $(".modal-content").removeClass(`${types.toLowerCase()}-bg`);
+      })
+
     }).catch(e => {
       console.error(e);
     });
   }
 
   // add pokemon items onto the page
-  function addListItem(pokemonItem){
+  function addListItem(pokemonItem) {
     let pokemonList = $(".pokemon-list");
     let listItem = $("<div class = 'list-group-item col-sm-6 col-md-4 col-xl-3'</div>");
     let button = $("button");
+
     button = $("<button>" + pokemonItem.name + "</button>");
+
     button.addClass("pokemonList-button");
 
     // add data toggle and data target to show modals
-    button.attr('data-toggle','modal');
-    button.attr('data-target', '#exampleModalCenter')
+    button.attr('data-toggle', 'modal');
+    button.attr('data-target', '#exampleModalCenter');
 
     listItem.append(button);
     pokemonList.append(listItem);
     // event listener
     button.on('click', () => {
-        showDetails(pokemonItem);
+      showDetails(pokemonItem);
+      console.log(pokemonItem.name);
     });
   }
 
-  function showDetails(pokemonItem){
+  function showDetails(pokemonItem) {
     pokemonRepository.loadDetails(pokemonItem).then(function () {
       console.log(pokemonItem);
       showModal(pokemonItem);
@@ -95,7 +109,7 @@ let pokemonRepository = (function (){
   }
 
   //Display modal
-  function showModal(pokemonItem){
+  function showModal(pokemonItem) {
     // modalContainer.innerHTML = '';
     let modalTitle = $(".modal-title");
     let modalBody = $(".modalBody");
@@ -109,30 +123,30 @@ let pokemonRepository = (function (){
     let pokemonImg = $('<img class = "pokemon-img img-fluid">');
     pokemonImg.attr('src', pokemonItem.imageUrl);
     pokemonImg.attr('alt', pokemonItem.name + ' image');
-    
+
     //description
-    let pokemonHeight = $("<p>" + 
-      "<b> Height: </b>" + 
-      pokemonItem.height + " ft" 
+    let pokemonHeight = $("<p>" +
+      "<b> Height: </b>" +
+      pokemonItem.height + " ft"
       + "</p>"
     );
 
-    let pokemonWeight = $("<p>" + 
-      "<b> Weight: </b>" + 
-      pokemonItem.weight + 
-      " lbs" 
+    let pokemonWeight = $("<p>" +
+      "<b> Weight: </b>" +
+      pokemonItem.weight +
+      " lbs"
       + "</p>"
     );
 
-    let pokemonTypes = $("<p>" + 
-      "<b> Types: </b>" + 
-      pokemonItem.types + 
+    let pokemonTypes = $("<p>" +
+      "<b> Types: </b>" +
+      pokemonItem.types +
       "</p>"
     );
 
-    let pokemonAbilities = $("<p>" + 
-      "<b> Abilities: </b>" + 
-      pokemonItem.abilities + 
+    let pokemonAbilities = $("<p>" +
+      "<b> Abilities: </b>" +
+      pokemonItem.abilities +
       "</p>"
     );
 
@@ -143,20 +157,20 @@ let pokemonRepository = (function (){
     modalBody.append(pokemonTypes);
     modalBody.append(pokemonAbilities);
 
-  } 
+  }
 
   // search function, credit goes to: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_filters_table
 
-  $(document).ready(function(){
-    $("#search").on("keyup", function() {
+  $(document).ready(function () {
+    $("#search").on("keyup", function () {
       var value = $(this).val().toLowerCase();
-      $(".pokemon-list *").filter(function() {
+      $(".pokemon-list *").filter(function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
     });
   });
 
-  return{
+  return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
@@ -167,25 +181,25 @@ let pokemonRepository = (function (){
 })();
 
 /////////// Print out Pokemon onto page ///////////////
-pokemonRepository.loadList().then(function() {
+pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(pokemonItem => {
-      pokemonRepository.addListItem(pokemonItem);
+    pokemonRepository.addListItem(pokemonItem);
   });
 });
 
 // scroll to top
 let btnTop = $('#btnTop');
 btnTop.on('click', () => {
-  $("html, body").animate({scrollTop: 0}, "slow");
+  $("html, body").animate({ scrollTop: 0 }, "slow");
 });
 
 // if scrolled down 20px from top of the doc, then show button 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btnTop.css({"display":"block"});
+    btnTop.css({ "display": "block" });
   } else {
-    btnTop.css({"display":"none"});
+    btnTop.css({ "display": "none" });
   }
 }
